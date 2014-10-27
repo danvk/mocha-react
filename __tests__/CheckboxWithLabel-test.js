75696e57c3f2341957602e99dbff53b1d279b554
Dan Vanderkam <danvdk@gmail.com>
2014-10-27 15:44:01 -0400

    Jest/React test demo

---
+++ __tests__/CheckboxWithLabel-test.js
@@ -0,0 +1,27 @@
+// __tests__/CheckboxWithLabel-test.js
+
+/** @jsx React.DOM */
+jest.dontMock('../CheckboxWithLabel.js');
+describe('CheckboxWithLabel', function() {
+  it('changes the text after click', function() {
+    var React = require('react/addons');
+    var CheckboxWithLabel = require('../CheckboxWithLabel.js');
+    var TestUtils = React.addons.TestUtils;
+
+    // Render a checkbox with label in the document
+    var checkbox = TestUtils.renderIntoDocument(
+      <CheckboxWithLabel labelOn="On" labelOff="Off" />
+    );
+
+    // Verify that it's Off by default
+    var label = TestUtils.findRenderedDOMComponentWithTag(
+      checkbox, 'label');
+    expect(label.getDOMNode().textContent).toEqual('Off');
+
+    // Simulate a click and verify that it is now On
+    var input = TestUtils.findRenderedDOMComponentWithTag(
+      checkbox, 'input');
+    TestUtils.Simulate.change(input);
+    expect(label.getDOMNode().textContent).toEqual('On');
+  });
+});
