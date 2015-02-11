984152d960688587950a4869542aac377f94c3fb
Dan Vanderkam <danvdk@gmail.com>
2015-02-11 12:19:59 -0500

    update jsdom API

--- tests/testdom.js
+++ tests/testdom.js
@@ -3,7 +3,7 @@ module.exports = function(markup) {
   if (typeof document !== 'undefined') return;
   var jsdom = require("jsdom").jsdom;
   global.document = jsdom(markup || '');
-  global.window = document.createWindow();
+  global.window = document.defaultView;
   global.navigator = {
     userAgent: 'node.js'
   };
